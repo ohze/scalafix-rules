6987315edb80f0a1c68775128dbef2c21deb9f5a
Bùi Việt Thành <thanhbv@sandinh.net>
2020-06-07 02:29:04 +0700

    Pending ExplicitNonNullaryApply2040 test
    Need update scalameta 4.3.12 for passing this test

--- scalafix/tests/src/test/scala/fix/RuleSuite.scala
+++ scalafix/tests/src/test/scala/fix/RuleSuite.scala
@@ -3,7 +3,15 @@ package fix
 import scalafix.testkit.SemanticRuleSuite
 
 class RuleSuite extends SemanticRuleSuite() {
-  val (pendingTests, tests) = testsToRun.partition(_.path.input.toFile.getName.endsWith("Pending.scala"))
+//  testsToRun
+//    .filter(_.path.input.toFile.getName.contains("2040.scala"))
+//    .foreach(runOn)
+
+  val (pendingTests, tests) = testsToRun.partition { t =>
+    val fileName = t.path.input.toFile.getName
+    fileName.endsWith("Pending.scala") || fileName.endsWith("2040.scala")
+  }
+
   tests.foreach(runOn)
   pendingTests.foreach { t =>
     test(t.path.testName) {
